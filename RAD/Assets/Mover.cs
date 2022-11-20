fc72b84bb2b2912e9efb1752a5c9145ec16d5a3d
kytice <77327418+kytice@users.noreply.github.com>
2022-11-20 22:57:16 +0000

    Add Walls

--- RAD/Assets/Mover.cs
+++ RAD/Assets/Mover.cs
@@ -4,19 +4,30 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField]float moveSpeed = 2f;
+    [SerializeField]float moveSpeed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-
-
+        PrintInstructions();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal")*Time.deltaTime * moveSpeed;
-        float zValue = Input.GetAxis("Vertical")*Time.deltaTime * moveSpeed;
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to my final project game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("And don't touch the walls"!);
+    }
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
     }
 }
