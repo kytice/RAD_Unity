310b7fa158f001b66b93836cdc23831d536f04cc
kytice <77327418+kytice@users.noreply.github.com>
2022-11-25 00:29:50 +0000

    Add Spinner script

---
+++ RAD/Assets/Scripts/Spinner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+
+    float xAngle = 0.3f;
+    float yAngle = 0;
+    float zAngle = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xAngle, yAngle, zAngle);
+    }
+}
